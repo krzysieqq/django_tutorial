0dfa2e05ffc6d4b2c3db47801b0f501c6e1f38f0
Krzysieqq <krzysi3qq@gmail.com>
2020-06-20 16:09:13 +0200

    Django tutorial part 2

--- app/django_tutorial/settings.py
+++ app/django_tutorial/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CUSTOM_APPS = [
+    'polls.apps.PollsConfig'
+]
+INSTALLED_APPS = BASE_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
